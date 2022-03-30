2efd4e02dfb7d442bacdb3ebb707a577aff8d1e6
jennymont <niacmonteiro@gmail.com>
2022-03-29 22:07:16 -0300

    alteracoes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div >
-    
+  <div>
+    <h1>Previsão do Tempo</h1>
+    <li class="pesquisa">
+      <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pela cidade" />
+      <button>Buscar</button>
+    </li>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  components: {},
+
+  data(){
+  return{
+    filtro: ''
+
   }
+},
+
+created(){
+  
 }
+};
+
+
 </script>
 
 <style>
+h1 {
+  text-align: center;
+  
+  font-size: 45px;
+  color:black;
+  height: 200px;
+  margin: 0;
+}
+.filtro {
+  
+  width: 70%;
+  border: 2px solid gray;
+  float: left;
+  margin-left: 15%;
+}
+
+.pesquisa{
+  margin-top: 0%;
+}
+
 *{
-  background-color: pink;
+  
 }
 </style>
