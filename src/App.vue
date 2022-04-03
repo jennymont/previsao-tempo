053bd8ca20c715a68b1ab6d3026c6d2770e10f26
jennymont <niacmonteiro@gmail.com>
2022-04-03 00:37:49 -0300

    atualizando

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,72 @@
 <template>
   <div>
-    <h1>Previsão do Tempo</h1>
+    <h1>
+      Previsão do Tempo
+      <img src="../imagens/img.png" alt="img.png" id="imagem1" />
+    </h1>
+
     <li class="pesquisa">
-      <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pela cidade" />
+      <input
+        type="search"
+        class="filtro"
+        v-on:input="filtro = $event.target.value"
+        placeholder="Filtre pela cidade"
+      />
+      {{ filtro }}
       <button>Buscar</button>
     </li>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
   components: {},
 
-  data(){
-  return{
-    filtro: ''
+  data() {
+    return {
+      filtro: "",
+    };
+  },
 
-  }
-},
-
-created(){
-  
-}
+  created() {
+    axios
+      .get("https://api.github.com/users/luizmedeiros00") //alterar api 
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
-
-
-</script>
+</script>t
 
 <style>
 h1 {
   text-align: center;
-  
+
   font-size: 45px;
-  color:black;
+  color: black;
   height: 200px;
   margin: 0;
 }
 .filtro {
-  
   width: 70%;
   border: 2px solid gray;
   float: left;
   margin-left: 15%;
 }
 
-.pesquisa{
+.pesquisa {
   margin-top: 0%;
 }
 
-*{
-  
+#imagem1 {
+  float: right;
+  width: 10%;
+  margin: 0.1em;
+  padding: 0.5em;
 }
 </style>
